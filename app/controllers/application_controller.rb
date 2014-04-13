831a655d42d72cfaf3cc5b68b2dcc6d99eec24cf
saleh139 <saleh139@yahoo.com>
2014-04-13 19:36:13 +0430

    debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || '/charge/dashboard'
   end
+  #protect_from_forgery with: :exception
 end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -19,7 +21,8 @@ module IE
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.default_locale = :fa
-    I18n.locale = :fa
+    #config.i18n.default_locale = :fa
+    #I18n.locale = :fa
+    #config.active_record.whitelist_attributes = true
   end
 end
