4614180cc9508b891bb6451588812c88fd3594c1
rghorbani <r.ghorbani.f@gmail.com>
2014-04-13 18:22:39 +0430

    new init

---
+++ app/controllers/charge_controller.rb
@@ -0,0 +1,7 @@
+class ChargeController < ApplicationController
+
+  before_action :authenticate_user!
+  
+  def dashboard
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IE2::Application.config.secret_key_base = '6b79954a8029fa8ab11ee97eed2a827685a4f1e377038520ebcae9c202aa7d31905d342b80ae7821f7cccd81852e86c421f54e97da78a8ff7b2f25ed65a715eb'
