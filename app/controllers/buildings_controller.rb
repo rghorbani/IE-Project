1fd57b2f047183e1328558ff07a7dfd7f571e49e
rghorbani <r.ghorbani.f@gmail.com>
2014-04-13 19:44:21 +0430

    fix some design problems

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,6 @@
 class BuildingsController < ApplicationController
+
+  before_action :authenticate_user!
   before_action :set_building, only: [:show, :edit, :update, :destroy]
 
   # GET /buildings
