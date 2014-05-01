d7b00aa210f344945ad0f028744ce52903cf1929
saleh139 <saleh139@yahoo.com>
2014-05-01 20:32:56 +0430

    Work on buildings and expenses and plans

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,34 @@ module ApplicationHelper
   def get_profile
   	@profile = current_user.profile || Profile.new
   end
+
+  def get_dir(params)
+    #buildings 0
+    #expenses 1
+    #plans 2
+    #index 0
+    #show 1
+    #edit 2
+    case params[:controller]
+      when 'buildings'
+        d1 = 0
+      when 'expenses'
+        d1 = 1
+      when 'plans'
+        d1 = 2
+      else
+        d1 = 3
+    end
+    case params[:action]
+      when 'index'
+        d0 = 0
+      when 'show'
+        d0 = 1
+      when 'edit'
+        d0 = 2
+      else
+        d0 = 3
+    end
+    return d0+10*d1
+  end
 end
