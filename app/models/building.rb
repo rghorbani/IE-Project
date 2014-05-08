98253ba0be87b38c1953993666b1b48e98acc650
rghorbani <r.ghorbani.f@gmail.com>
2014-05-09 01:39:46 +0430

    cleaning modals

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,5 +3,6 @@ class Building < ActiveRecord::Base
 	has_many :units
 	has_many :expenses
 	has_many :plans
+	has_many :messages
 	validates_presence_of :units_cnt, :message => "ورود تعداد واحد ساختمان الزامی است."
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 	has_many :units
   has_many :payments
   has_many :news
+  has_and_belongs_to_many :messages
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
