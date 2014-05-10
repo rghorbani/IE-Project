d4976c1e977da58ccda2f1060634be819b305130
ubuntu 12.04 II <farlin@ubuntu.(none)>
2014-05-10 13:32:36 -0700

    changes in UI and some bug fixes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  before_action :ban_resident_area
+  before_action :ban_resident_area, only: [:index]
   before_action :authenticate_user!
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
 
